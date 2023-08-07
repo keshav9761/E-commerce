701c6213266d3f32dd97b42feab82c9695bb0f9f
Keshav Tomar <keshavtomar53@gmail.com>
2023-08-07 16:35:25 +0530

    pagination working client

--- src/Pages/Products/ProductList.js
+++ src/Pages/Products/ProductList.js
@@ -6,9 +6,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { StarIcon } from '@heroicons/react/24/outline'
 import Pagination from '../Pagination'
 export default function ProductList() {
-
+  const [page, setPage] = useState(1)
   const [searchQuery, setSearchQuery] = useState("");
-  // const { products = [] } = useSelector((state) => state?.product.data) || {};
   const products = useSelector((state) => state?.product?.data || [])
 
   // console.log("777##", products)
@@ -17,6 +16,13 @@ export default function ProductList() {
   const handleSearch = (event) => {
     setSearchQuery(event.target.value);
   };
+  const selectedPageHandler = (selectedPage) => {
+    if (selectedPage >= 1 &&
+      selectedPage <= (products.length / 10) &&
+      selectedPage !== page
+    )
+      setPage(selectedPage)
+  }
 
   useEffect(() => {
     console.log("first",)
@@ -48,50 +54,75 @@ export default function ProductList() {
               ) {
                 return item;
               }
-            })?.map((product, i) => (
-              <Link to={`/productdetail/${product.id}`}>
-                <div key={product.id} className="group relative border-solid border-2 p-2 border-gray-200">
-                  <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80" id='box'>
-                    <img
-                      src={product?.thumbnail}
-                      // alt={product.thumbnail}
-                      className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                    />
-                  </div>
-                  <div className="mt-4 flex justify-between">
-                    <div>
-                      <h3 className="text-sm text-gray-700">
-                        <div>
-                          <span aria-hidden="true" className="" />
-                          {product?.title}
-                        </div>
-                      </h3>
-                      {/* <p className="mt-1 text-sm text-gray-500">
+            })?.
+              slice(page * 10 - 10, page * 10)
+              .map((product, i) => (
+                <Link to={`/productdetail/${product.id}`}>
+                  <div key={product.id} className="group relative border-solid border-2 p-2 border-gray-200">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80" id='box'>
+                      <img
+                        src={product?.thumbnail}
+                        // alt={product.thumbnail}
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <div>
+                            <span aria-hidden="true" className="" />
+                            {product?.title}
+                          </div>
+                        </h3>
+                        {/* <p className="mt-1 text-sm text-gray-500">
                       <StarIcon className='w-6 h-6 inline'></StarIcon>
                       <span className='mt-11'></span>{product.rating}
                       </p> */}
-                      <p className="mt-1 text-sm text-gray-500">
-                        {product.brand}
-                      </p>
+                        <p className="mt-1 text-sm text-gray-500">
+                          {product.brand}
+                        </p>
+                      </div>
+                      <div>
+                        <p className="text-sm font-medium text-gray-400 line-through decoration-red-700">
+                          Rs.{product.price}
+                        </p>
+                        <p className="text-sm font-medium text-gray-900">
+                          Dis.{Math.round(product?.price * (1 - product?.discountPercentage / 100))}
+                        </p>
+                      </div>
                     </div>
-                    <div>
-                      <p className="text-sm font-medium text-gray-400 line-through decoration-red-700">
-                        Rs.{product.price}
-                      </p>
-                      <p className="text-sm font-medium text-gray-900">
-                        Dis.{Math.round(product?.price * (1 - product?.discountPercentage / 100))}
-                      </p>
+                    <div className="flex flex-col items-center justify-center">
                     </div>
                   </div>
-                  <div className="flex flex-col items-center justify-center">
-                  </div>
-                </div>
-              </Link>
-            ))}
+                </Link>
+              ))}
           </div>
         </div>
-      {/*--------------------- pagination--------------------------- */}
-        <Pagination />
+        {/*--------------------- pagination--------------------------- */}
+        {/* <Pagination /> */}
+        {
+          products.length > 0 && <div className='pagination'>
+            <span
+              className={page > 1 ? "" : "pagination_disable"}
+              onClick={() => selectedPageHandler(page - 1)}
+            >
+              Pre</span>
+            {
+              [...Array(products.length / 10)].map((_, i) =>
+                <span
+                  key={i}
+                  onClick={() => selectedPageHandler(i + 1)}
+                  className={page === i + 1 ? "pagination_selected" : ""}
+                >{i + 1}</span>)
+            }
+            <span
+              onClick={() => selectedPageHandler(page + 1)}
+              className={page < products.length / 10 ? "" : "pagination_disable"}
+
+            >
+              Next</span>
+          </div>
+        }
       </div>
 
     </>
